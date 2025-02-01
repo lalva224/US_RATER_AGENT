3cf523c69efb7d0ad3cb1c1344766c97306fb748
Leo <alvarezleandro720@gmail.com>
2025-02-01 18:53:03 -0500

    using janus pro model, but need to move to google colab for GPU access

--- agent.py
+++ agent.py
@@ -10,6 +10,11 @@ import json
 import torch
 from PIL import Image
 from lib.utlity import scrape_page,capture_desktop_and_mobile_screenshots
+from transformers import AutoProcessor, AutoModelForCausalLM
+import torch
+from transformers import AutoModelForCausalLM
+from janus.models import MultiModalityCausalLM, VLChatProcessor
+from janus.utils.io import load_pil_images
 
 load_dotenv()
 
@@ -30,10 +35,52 @@ def evaluate_page(website):
     mobile_desktop_screenshots = capture_desktop_and_mobile_screenshots(website)
     desktop_start, desktop_mid = mobile_desktop_screenshots['desktop']
     mobile_start, mobile_mid = mobile_desktop_screenshots['mobile']
-    desktop_start.show()
-    desktop_mid.show()
-    mobile_start.show()
-    mobile_mid.show()
+
+    images = [desktop_start,desktop_mid,mobile_start,mobile_mid]
+    question = 'Evaluate these website screenshots and describe the user experience, design quality, and content organization. For the mobile screenshots note if the scaling or designing is off.'
+
+    # specify the path to the model
+    model_path = "deepseek-ai/Janus-Pro-7B"
+    vl_chat_processor: VLChatProcessor = VLChatProcessor.from_pretrained(model_path)
+    tokenizer = vl_chat_processor.tokenizer
+
+    vl_gpt: MultiModalityCausalLM = AutoModelForCausalLM.from_pretrained(
+        model_path, trust_remote_code=True
+    )
+    vl_gpt = vl_gpt.to(torch.bfloat16).cuda().eval()
+
+    conversation = [
+        {
+            "role": "<|User|>",
+            "content": f"<image_placeholder>\n{question}",
+            "images": images,
+        },
+        {"role": "<|Assistant|>", "content": ""},
+    ]
+
+    # load images and prepare for inputs
+    pil_images = load_pil_images(conversation)
+    prepare_inputs = vl_chat_processor(
+        conversations=conversation, images=pil_images, force_batchify=True
+    ).to(vl_gpt.device)
+
+    # # run image encoder to get the image embeddings
+    inputs_embeds = vl_gpt.prepare_inputs_embeds(**prepare_inputs)
+
+    # # run the model to get the response
+    outputs = vl_gpt.language_model.generate(
+        inputs_embeds=inputs_embeds,
+        attention_mask=prepare_inputs.attention_mask,
+        pad_token_id=tokenizer.eos_token_id,
+        bos_token_id=tokenizer.bos_token_id,
+        eos_token_id=tokenizer.eos_token_id,
+        max_new_tokens=512,
+        do_sample=False,
+        use_cache=True,
+    )
+
+    answer = tokenizer.decode(outputs[0].cpu().tolist(), skip_special_tokens=True)
+    print(f"{prepare_inputs['sft_format'][0]}", answer)
 
 def page_quality_rating(website):
     #first try with just 1 reasoning layer, plugging in scraped website text and research from google ground api
@@ -60,8 +107,7 @@ def page_quality_rating(website):
     )
     return completion.choices[0].message
 
-evaluate_page('https://huggingface.co/deepseek-ai/Janus-Pro-7B')
-
+# evaluate_page('https://huggingface.co/deepseek-ai/Janus-Pro-7B')
 
 
     
